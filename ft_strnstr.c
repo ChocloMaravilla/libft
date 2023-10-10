0bf0ee7f70d73df2bdd4412f05ec08ada2a9d032
ruben <ruskymartinez@gmail.com>
2023-10-10 16:52:57 +0200

    backup

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,20 +6,39 @@
 /*   By: ruben <ruben@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/08 19:03:50 by ruben             #+#    #+#             */
-/*   Updated: 2023/10/08 19:04:42 by ruben            ###   ########.fr       */
+/*   Updated: 2023/10/10 16:52:04 by ruben            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-ft_strnstr()
+char *ft_strnstr(const char *big, const char *little, size_t len)
 {
+    size_t count;
+
+    // little = (char *)little;
+    // big = (char *)big;
+    count = 0;
+    while(count < len && little[count])
+    {
+        if(big[count] == little[count] && !(little[count] == '\0'))
+            return (char *)little;
         
+        count++;
+    }
+    if(!little)
+        return (char *)big; 
+    return NULL;
 }
 
 int main()
 {
+    char *str = "Buenos dias";
+    char *cha = "di";
+    char *result;
+    size_t len = ft_strlen(str);
     
-    ft_strnstr();
+    result = ft_strnstr(str, cha, len);
+    printf("%s", result);
     return 0;    
 }
