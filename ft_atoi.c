f80564e0bbc55ab839e582138025af4d4ac29e3d
ruben <ruskymartinez@gmail.com>
2023-10-12 23:02:25 +0200

    atoi2

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,41 +6,42 @@
 /*   By: ruben <ruben@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/12 17:51:23 by ruben             #+#    #+#             */
-/*   Updated: 2023/10/12 18:57:57 by ruben            ###   ########.fr       */
+/*   Updated: 2023/10/12 23:01:27 by ruben            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h" 
 
-int ft_atoi (const char * str)
+int ft_atoi (const char *str)
 {
-    // int result;
-    // char *striing;
-    // int temp;
-    // // int limit;
-    // size_t  len;
-    // size_t count;
+    int a;
+    int nb;
+    int sign;
 
-    // //limit = 50;
-    // count = 0;
-    // striing = (char *)str;
-    // len = ft_strlen(str);
-    // while(count < len - 1)
-    // {
-    //     result = (int) string[count];
-    //     result  - limit + 2; 
-    //     count++;
-    // }    
-    // return result;
-
+    sign = 1;
+    a = 0;
+    nb = 0;
     
+    if(str[a] == ' ' || str[a] == '\n' || str[a] == '\t' || str[a] == '\r' || str[a] == '\f' || str[a] == '\v')
+        a++;
+    if(str[a] == '-')
+        {sign = -1;
+        a++;
+        }
+    if (str[a] == '+')
+        a++;
+    while(str[a] != '\0')
+    {
+        nb = (nb * 10) + (str[a] - '0');
+        a++;    
+    }
+    return (nb * sign);
 }
 
-int main()
-{
-    char *str = "6";
-    int result;
-    result = ft_atoi(str);
-    printf("%i", result);
-    return 0;    
-}
+// int main()
+// {
+//     char *str = "7-2";
+//     int result = ft_atoi(str);
+//     printf("%d", result);
+//     return 0;    
+// }
