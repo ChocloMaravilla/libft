e1c9598ad626679280b2e463038979f5bda91804
Ruben Medina Martinez <rmedina-@car10s1.42barcelona.com>
2024-05-11 19:51:20 +0200

    push

--- ft_lstadd_back_bonus.c
+++ ft_lstadd_back_bonus.c
@@ -1,35 +1,22 @@
 void ft_lstadd(t_list **lst, t_list *new)
 {
+	t_list *head;
+
+	head = *lst;
 	if (!*lst)
 	{	
+		new->next = *lst;
+		*lst = new;
 		return ;
 	}
-
+	while(head)
+	{
+		if(head->next == NULL)
+		{	
+			head->next = new;
+			return;
+		}
+		head = head->next;
+	}
 	return ;
 }
-t_list create_node(void *content)
-{
-	t_list *head;
-
-	head = malloc(sizeof(t_list));
-	head->content = content;
-	head->next = NULL;
-	return (head);
-}
-int main(int argc, char **argv)
-{
-	t_list *head;
-	t_list *new;
-	int i;
-
-	if(argc != 2)
-		return (1);
-	i = 0;
-	while(i < argc)
-	{
-		new = create_node(argv[1]);
-		ft_lstadd(&head, new);
-		i++;
-	}
-	return (0);
-};
